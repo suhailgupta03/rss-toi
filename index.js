c126e5f6bcd439231a2ac501665c6ccd4997d1b3
Cl-mac-mini-81 <cl-mac-mini-81@admins-Mac-mini.local>
2017-06-06 14:52:53 +0530

    adds download options

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+require('dotenv').config();
+
+
+var connection = require('mysql').createConnection({
+  host     : 'localhost',
+  user     : process.env.USERNAME || 'root',
+  password : process.env.PASSWORD || '',
+  database : process.env.DATABASE || 'toi'
+});
+
+global.connection = connection;
+
 require('./lib/reader').start();

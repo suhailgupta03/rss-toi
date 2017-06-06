cc83528cbaa45dd882c4d831a67de801167fc41f
suhailgupta03 <suhailgupta03@gmail.com>
2017-06-06 09:04:43 +0530

    Adds pipe

---
+++ util/constant.js
@@ -0,0 +1,9 @@
+module.exports = class Constant {
+    
+    /**
+     * Path to URL that will be parsed
+     */
+    static get URL() {
+        return 'http://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms';
+    }
+}
